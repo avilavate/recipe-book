96d79fd0aea8f56930ee2aef1d39f84affc340ef
avilavate <avilavate123@gmail.com>
2017-03-15 14:17:34 +0530

    Added associated components for add and edit recipe routes.

---
+++ src/app/edit-recipe/edit-recipe.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rb-edit-recipe',
+  template: `
+    <p>
+      edit-recipe Works!
+    </p>
+  `,
+  styles: []
+})
+export class EditRecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/new-recipe/new-recipe.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rb-new-recipe',
+  template: `
+    <p>
+      new-recipe Works!
+    </p>
+  `,
+  styles: []
+})
+export class NewRecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
