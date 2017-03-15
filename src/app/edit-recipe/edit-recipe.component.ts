e0874ef94226f6515d3a83924dfc2716ad7ba76a
avilavate <avilavate123@gmail.com>
2017-03-15 17:17:40 +0530

    Added edit-recipe submit route.

--- src/app/edit-recipe/edit-recipe.component.ts
+++ src/app/edit-recipe/edit-recipe.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
-import {RecipesService} from '../shared'
+import { RecipesService } from '../shared'
 
 @Component({
   selector: 'rb-edit-recipe',
-  templateUrl:'./edit-recipe.component.html',
+  templateUrl: './edit-recipe.component.html',
   styles: []
 })
-export class EditRecipeComponent  {
-
-  constructor(public recipeService:RecipesService) { }
-
+export class EditRecipeComponent {
+  constructor(public recipeService: RecipesService, public router: Router) { }
+  onSubmit(f: NgForm) {
+    if (f.valid)
+      this.router.navigate(["recipe-list"]);
+  }
 }
