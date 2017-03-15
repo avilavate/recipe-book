c9372a1ef945c93dcf4e2f46e42fa42edfa0bb82
avilavate <avilavate123@gmail.com>
2017-03-15 17:04:37 +0530

    Added ngForm for new Recipe.

--- src/app/new-recipe/new-recipe.component.ts
+++ src/app/new-recipe/new-recipe.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class NewRecipeComponent implements OnInit {
 
   constructor() { }
-
+  onSubmit(form) {
+    console.dir(form);
+  }
   ngOnInit() {
   }
 
