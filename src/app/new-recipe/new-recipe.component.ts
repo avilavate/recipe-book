4fd27fb680cacf8af8f421c8aae69d7a4fa14038
avilavate <avilavate123@gmail.com>
2017-03-31 15:39:39 +0530

    Fixed firebase image problems fixed default recipe image. (Todo: Require fix).

--- src/app/new-recipe/new-recipe.component.ts
+++ src/app/new-recipe/new-recipe.component.ts
@@ -10,12 +10,15 @@ import { Http } from "@angular/http";
   templateUrl: './new-recipe.component.html'
 })
 export class NewRecipeComponent {
+  private fireBaseUrl: string = "https://recipe-book-264a0.firebaseio.com/data.json";
   newRecipe: Recipe;
+
   constructor(private http: Http, public router: Router, public recipesService: RecipesService) { }
   onSubmit(form: NgForm) {
     if (!form.valid) return;
-    this.newRecipe = new Recipe(form.value.recipeName, form.value.recipeDesc, null, []);
-    this.http.post("https://recipe-book-264a0.firebaseio.com/data.json", this.newRecipe).subscribe(
+    form.value.recipeImage = form.value.recipeImage == "" ? '../../assets/images/kebab.jpg' : form.value.recipeImage;
+    this.newRecipe = new Recipe(form.value.recipeName, form.value.recipeDesc, form.value.recipeImage, []);
+    this.http.post(this.fireBaseUrl, this.newRecipe).subscribe(
       data => {
         console.log("Success: " + data);
         //Temperory fix ToDo: Remove this code later
--- src/app/shared/recipes.service.ts
+++ src/app/shared/recipes.service.ts
@@ -7,10 +7,11 @@ export class RecipesService {
   private recipes: Recipe[] = [];
   constructor() {
 
-    this.recipes.push(new Recipe('Tometo Salad', 'Salad is good for health if its not tossed', './app/recipe-list/images/tomato_salad.jpg',
+    this.recipes.push(new Recipe('Tometo Salad', 'Salad is good for health if its not tossed', '../../assets/images/tomato_salad.jpg',
       new Array<Ingradients>(new Ingradients("Tomato", 2), new Ingradients("Olive Oil", "10 ml"))));
-    this.recipes.push(new Recipe('Biryani', 'Steamed rice with saffron and choice of chicked/mutton', './app/recipe-list/images/biryani.jpg',
+    this.recipes.push(new Recipe('Biryani', 'Steamed rice with saffron and choice of chicked/mutton', '../../assets/images/biryani.jpg',
       new Array<Ingradients>(new Ingradients("Rice", "2 kg"), new Ingradients("Chicken", "1.5 kg"))));
+      
   }
   getRecipes(): Recipe[] {
     return this.recipes;
