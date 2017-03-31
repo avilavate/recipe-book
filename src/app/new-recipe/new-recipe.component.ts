8eb1324676244bc8b9be485a6c504f06df86be03
avilavate <avilavate123@gmail.com>
2017-03-31 11:38:10 +0530

    Itegrated google firebase for adding new recipes to firebase db.

--- src/app/new-recipe/new-recipe.component.ts
+++ src/app/new-recipe/new-recipe.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Recipe } from "app/shared";
+import { Http } from "@angular/http";
 
 @Component({
   selector: 'rb-new-recipe',
-  templateUrl: './new-recipe.component.html',
-  styles: []
+  templateUrl: './new-recipe.component.html'
 })
-export class NewRecipeComponent implements OnInit {
-
-  constructor() { }
-  onSubmit(form) {
-    console.dir(form);
+export class NewRecipeComponent  {
+  newRecipe: Recipe;
+  constructor(private http: Http,  public router:Router) { }
+  onSubmit(form: NgForm) {
+    if (!form.valid) return;
+    this.newRecipe = new Recipe(form.value.recipeName, form.value.recipeDesc, null, []);
+    this.http.post("https://recipe-book-264a0.firebaseio.com/data.json", this.newRecipe).subscribe(
+      data => { console.log("Success: " + data) },
+      error => { console.log(error) },
+      () => { console.log("request completed!") });
+      this.router.navigate(["/"]);
   }
-  ngOnInit() {
-  }
-
 }
